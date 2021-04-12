dc53dc010eeeb0582e8fe5b0ccedf5f783d713e2
lavkayV <lavkay2@gmail.com>
2021-04-12 16:58:08 +0300

    autotest

---
+++ ActPro/src/main/java/Hello.java
@@ -0,0 +1,8 @@
+public class Hello {
+    public static void main (String[] args) {
+        int a=5;
+        int b=48;
+        int c= (a+b) * 36;
+        System.out.println("Hello world!" + "The variable c = " + c);
+    }
+}
